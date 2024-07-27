2a9a8074242d934a82c241f12789023dc5f1a468
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-27 14:53:35 +0545

    [check]: User ip address checked

--- index.js
+++ index.js
@@ -6,70 +6,53 @@ const rateLimiter = require('./limitation/rateLimiter')
 
 app.use('/api/', rateLimiter);
 
+// app.get('/', (req, res) => {
+//     res.send('Hello, World!')
+// })
+
+
+
+
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!')
+    console.log('Incoming request IP:', req.ip);
+    const first = req.headers['x-forwarded-for']
+const second = req.connection.remoteAddress
+   console.log('first',first)
+console.log('second',second)
+    res.send('User data!')
 })
 
-app.get('/product', async(req, res) => {
-    let post
-    if(client.isReady){
-        post = await client.get('post-data')
-    }
-    if(post){
-        console.log("Cache hit")
-        return res.send(JSON.parse(post))
-    }else{
-        console.log("Cache miss")
-        post = await axios.get('https://jsonplaceholder.typicode.com/posts')
-        if(client.isReady){
-            client.setEx('post-data', 10, JSON.stringify(post.data))  // cache for 1 hour
-        }
-        res.send(post.data)
-    }
-})
+// app.get('/api/payment', (req, res) => {
+//     res.send('product data!')
+// })
+
+// app.get('/api/product', async(req, res) => {
+//     let counter = 0
+//     for(i=0; i<1000000000; i++){
+//       counter++
+//     }
+//     res.send('Rate limiting is working!');
+// })
+
+    // let post
+    // if(client.isReady){
+    //     post = await client.get('post-data')
+    // }
+    // if(post){
+    //     console.log("Cache hit")
+    //     return res.send(JSON.parse(post))
+    // }else{
+    //     console.log("Cache miss")
+    //     post = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    //     if(client.isReady){
+    //         client.setEx('post-data', 10, JSON.stringify(post.data))  // cache for 1 hour
+    //     }
+    //     res.send(post.data)
+    // }
 
 // LIST
-// async function runRedisListExample() {
-//     try {
-//         // Clear the list if it exists
-//         await client.del('mylist');
 
-//         // Push elements to the list (left push)
-//         await client.lPush('mylist', 'one');
-//         await client.lPush('mylist', 'two');
-//         await client.lPush('mylist', 'three');
-//         await client.lPush('mylist', 'ten');
-
-//         // Push elements to the list (right push)
-//         await client.rPush('mylist', 'four');
-//         await client.rPush('mylist', 'five');
-
-//         // Get all elements from the list
-//         const listItems = await client.lRange('mylist', 1,3);
-//         console.log('List items:', listItems);
-
-//         // // Pop an element from the left of the list
-//         const leftPoppedItem = await client.lPop('mylist');
-//         console.log('Left popped item:', leftPoppedItem);
-
-//         const length = await client.lLen('mylist');
-//         console.log('length of my list ', length);
-
-//         // Pop an element from the right of the list
-//         const rightPoppedItem = await client.rPop('mylist');
-//         console.log('Right popped item:', rightPoppedItem);
-
-//         // Get the updated list
-//         const updatedListItems = await client.lRange('mylist', 0, -1);
-//         console.log('Updated list items:', updatedListItems);
-//     } catch (err) {
-//         console.error('Error running Redis list example:', err);
-//     } finally {
-//         // Close the Redis client
-//         await client.quit();
-//     }
-// }
-// runRedisListExample()
 
 
 // SET
