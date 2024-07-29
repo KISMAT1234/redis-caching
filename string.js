53a2f50c7d6f1dcd0b66fc6e6ba0447fb369b1a7
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-29 14:30:04 +0545

    adding value using mset and incr

--- string.js
+++ string.js
@@ -10,16 +10,36 @@ console.log('kismat bhai siuu')
 const string = async() => {
     try{
 
-        const res1 = await client.set("bike:1", "Deimos");
-        console.log(res1,'res1');  // OK
-        const res2 = await client.get("bike:1");
-        console.log(res2,'res2');  // Deimos
+        // const res1 = await client.set("bike:1", "Deimos");
+        // console.log(res1,'res1');  // OK
+        // const res2 = await client.get("bike:1");
+        // console.log(res2,'res2');  // Deimos
 
-        await client.set("plane-crash",105)
-        const result = await client.incr("plane-crash", 1)
-        console.log(result)
-        const final = await client.get("plane-crash")
-        console.log(final,'final')
+        // await client.set("plane-crash",105)
+        // const result = await client.incr("plane-crash", 1)
+        // console.log(result)
+        // const final = await client.get("plane-crash")
+        // console.log(final,'final')
+
+        // const mcet = await client.mSet([
+        //     ["school","hari"],
+        //     ["sports","krishna"],
+        //     ["hotel","esha"]
+        // ])
+        // console.log(mcet,'mcet')
+        // const mgat = await client.mGet(["school","hotel","sports"])
+        // console.log(mgat,'mgat')
+
+        // const achievement = await client.set("medal",5)
+        // console.log(achievement,'achievement')
+
+        // const increment = await client.incrBy("medal",4) 
+        // console.log(increment,'increment')
+
+        const decrement = await client.decrBy("medal",5) 
+        console.log(decrement,'decrement')
+
+        
     }
      catch (err) {
             console.error('Error running Redis list example:', err);
