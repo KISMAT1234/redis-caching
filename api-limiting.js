f17bf560dcd01d9fb2e44af7019072f5629658a8
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-31 17:13:56 +0545

    returning middleware function if it is true

--- api-limiting.js
+++ api-limiting.js
@@ -1,18 +1,28 @@
 const client = require('./redis')
-async function rateLimiter(req,res,next){
 
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress.slice(0,9)
-    console.log('ip',ip)
-
-    const request = await client.incr(ip)
-    console.log(request,' number of request ')
-
-    let ttl
-    if(request === 1){
-       await client.expire(ip,60)
-       ttl=60
-    }else {
-        ttl = await client.ttl(ip)
+ function apiLimiter({seconds, allowedHits}){
+    return async function (req, res, next) {
+        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress.slice(0,9)
+        console.log('ip',ip)
+    
+        const request = await client.incr(ip)
+        console.log(request,' number of request ')
+    
+        let ttl
+        if(request === 1){
+           await client.expire(ip,seconds)
+           ttl=60
+        }else {
+            ttl = await client.ttl(ip)
+        }
+        if(request > allowedHits){
+            console.log(ttl,'time taken')
+            return res.status(503).json({message:'To many request'})
+        }else{
+            next()
+        }
     }
+
+   
 }
-module.exports = rateLimiter
+module.exports = apiLimiter
