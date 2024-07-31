94c32e42cdc18497cd105e08aea91a89785954ca
KISMAT1234 <kismatbohora123@gmail.com>
2024-07-31 17:42:45 +0545

    list check

--- list.js
+++ list.js
@@ -12,13 +12,19 @@ async function runRedisListExample() {
         //     'bikes:repairs', ['bike:1', 'bike:2', 'bike:3', 'bike:4', 'bike:5']
         //   );
         //   console.log(res27eol);  // 5
-        const poped =await client.rPop('bikes:repairs')
-        console.log(poped);
-        const a = await client.rLen('pilot:plane')
-        console.log(a)
-        const res13 = await client.lRange('pilot:plane', 0,-1);
-        console.log(res13);  // ['bike:1']    
-        //rpush
+        // const poped =await client.rPop('bikes:repairs')
+        // console.log(poped);
+        // const a = await client.rLen('pilot:plane')
+        // console.log(a)
+        // const res13 = await client.lRange('pilot:plane', 0,-1);
+        // console.log(res13);  // ['bike:1']    
+        
+        // const v = await client.lTrim()
+
+        const b = await client.lPop('pilot:plane')
+        console.log(b);
+        const res49 = await client.lTrim('bikes:repairs', 0, 2);
+        console.log(res49);  // 'OK'
 
     } catch (err) {
         console.error('Error running Redis list example:', err);
