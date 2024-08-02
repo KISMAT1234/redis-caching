fe992daeaa1be7a946d780a25a915c9284f6f05c
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-02 18:11:34 +0545

    configuring pricecalculator class

--- SOLID/S.js
+++ SOLID/S.js
@@ -6,10 +6,8 @@ const product2 = new Product('2','Mobile', 50)
 
 const order = new Order();
 const pricing = new priceCalculator()
-pricing.calculatePricing(order.getProducts())
 
-
-
+let total = pricing.calculatePricing(order.getProducts())
 order.addProduct(product2)
 order.addProduct(product1)
 order.generateInvoice()
