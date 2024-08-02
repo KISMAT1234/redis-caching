d71d68db3a55fd66ece68c6729e544393604723a
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-02 18:10:48 +0545

    checking ip value

--- api-limiting.js
+++ api-limiting.js
@@ -2,7 +2,9 @@ const client = require('./redis')
 
  function apiLimiter({seconds, allowedHits}){
     return async function (req, res, next) {
-        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress.slice(0,9)
+        // const i = (req.headers['x-forwarded-for'] || req.connection.remoteAddress)
+        // console.log(i)
+        const ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress).slice(0,9)
         console.log('ip',ip)
     
         const request = await client.incr(ip)
@@ -11,10 +13,13 @@ const client = require('./redis')
         let ttl
         if(request === 1){
            await client.expire(ip,seconds)
-           ttl=60
+           ttl = 60
         }else {
             ttl = await client.ttl(ip)
+            console.log(ttl,'time taken')
+
         }
+
         if(request > allowedHits){
             console.log(ttl,'time taken')
             return res.status(503).json({message:'To many request'})
