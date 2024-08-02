13a5a2d79e5e198e0f168a0684f3c6805d4deca7
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-02 15:37:15 +0545

    initialized calculating price

--- SOLID/S.js
+++ SOLID/S.js
@@ -1,11 +1,16 @@
 import { Product,Order } from "./order.js";
+import priceCalculator from "./priceCalculator.js";
 
 const product1 = new Product('1','Laptop', 100)
 const product2 = new Product('2','Mobile', 50)
 
 const order = new Order();
+const pricing = new priceCalculator()
+pricing.calculatePricing(order.getProducts())
+
+
+
 order.addProduct(product2)
 order.addProduct(product1)
-
 order.generateInvoice()
 order.processPayment()
