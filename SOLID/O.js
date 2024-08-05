f00d14172f4ee4ae455bb828e40994a7f8fa0f14
KISMAT1234 <kismatbohora123@gmail.com>
2024-08-05 14:52:28 +0545

    success on creating different class of payment type using open closed principle

--- SOLID/O.js
+++ SOLID/O.js
@@ -1,13 +1,60 @@
+// ***************** BAD WAY *************************
+
+// class PaymentProcessor {
+
+//     processPayment(amount, paymentType){
+//      if(paymentType === 'esewa'){
+//     console.log('payment of ',paymentType)
+// } 
+// else if(paymentType === 'khalti'){
+//     console.log('payment of ',paymentType)
+// } 
+// else if(paymentType === 'imepay'){
+//     console.log('payment of ',paymentType)
+// } else {
+//     throw new Error('Unknown payment error')
+// }
+//     }
+// }
+// const processor = new PaymentProcessor()
+// processor.processPayment(100,'esewa')
+
+
+
+
+
+
+
+
+
+// ********************* GOOD WAY *************************
+
 class PaymentProcessor {
 
     constructor(paymentProcessor) { 
-        this.processor = paymentProcessor; 
-    }
+       this.processor = paymentProcessor; 
+    } 
 
-    processPayment(amount, paymentType){
-        console.log(amount,`amount value ${amount} by payment type ${paymentType}`); 
+    processPayment(amount){
+        this.processor.processPayment(amount) 
     }
 }
 
-const processor = new PaymentProcessor()
-processor.processPayment(100,'paypal')
+class EsewaProcessor {
+    processPayment(amount){
+       console.log('processing esewa payment in esewa worth',amount)
+    }
+}
+
+class KhaltiProcessor {
+    processPayment(amount){
+       console.log('processing khalti payment in esewa worth',amount)
+    }
+}
+
+
+
+const Esewa  = new EsewaProcessor()
+const Khalti = new KhaltiProcessor()
+const processor = new PaymentProcessor(Khalti)
+processor.processPayment(100)
